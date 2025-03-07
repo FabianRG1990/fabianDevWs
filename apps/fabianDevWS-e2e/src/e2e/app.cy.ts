d2dca0eaf9ff3f14f1aafde520a52d1e2dd69046
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-03-06 22:35:08 -0600

    feat: app.cy.ts

--- apps/fabianDevWS-e2e/src/e2e/app.cy.ts
+++ apps/fabianDevWS-e2e/src/e2e/app.cy.ts
@@ -1,12 +1,6 @@
-import { getGreeting } from '../support/app.po';
 
 describe('fabianDevWS-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
-  });
 });
